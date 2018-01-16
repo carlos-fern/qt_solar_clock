54086ac99fc4e207796d162c37170e84f00e0276
Carlos <carlos@electricmovement.com>
2018-01-16 08:17:52 -0800

    fixed dependency handling

--- builder.sh
+++ builder.sh
@@ -1,6 +1,4 @@
-#!/bin/bash
-
-usage="builds the qt_solar_clock and handles the QT 5.5 depedency"
+age="builds the qt_solar_clock and handles the QT 5.5 depedency"
 PROJ_DIR=$(pwd)
 echo "Creating build env"
 mkdir -p build/qt_resources
@@ -14,33 +12,55 @@ if [ ! -d "qt-everywhere-opensource-src-5.5.0" ]; then
 	tar -xzf qt-everywhere-opensource-src-5.5.0.tar.gz
 fi
 
-if [ ! -d "libxcb-1.12" ]; then
-	echo "Downloading libxcb 1.12"
-	wget "https://xcb.freedesktop.org/dist/libxcb-1.12.tar.gz"
-	tar -xzf libxcb-1.12.tar.gz
+
+if [ ! -d "macros" ]; then
+	git clone "git://anongit.freedesktop.org/git/xorg/util/macros"
 fi
 
-if [ ! -d "xcb-proto-1.12" ]; then
-	echo "Downloading xcb-prot 1.12"
-	wget "https://xcb.freedesktop.org/dist/xcb-proto-1.12.tar.gz"
-	tar -xzf xcb-proto-1.12.tar.gz
+if [ ! -d "libXau" ]; then
+	git clone "git://anongit.freedesktop.org/git/xorg/lib/libXau"
+fi
+
+if [ ! -d "x11proto" ]; then
+	git clone "git://anongit.freedesktop.org/git/xorg/proto/x11proto"
+fi
+
+if [ ! -d "pthread-stubs" ]; then
+	git clone "git://anongit.freedesktop.org/git/xcb/pthread-stubs"
+fi
+
+if [ ! -d "proto" ]; then
+	git clone "git://anongit.freedesktop.org/git/xcb/proto"
+fi
+
+if [ ! -d "libxcb" ]; then
+	git clone "git://anongit.freedesktop.org/git/xcb/libxcb"
 fi
 
 
-cd xcb-proto-1.12
-echo "Building xcb-proto 1.12"
-./configure
-make
+cd macros
+./autogen.sh
 make install
 
-cd $QT_DIR/libxcb-1.12
-echo "Building libxcb 1.12"
-./configure
-make
+cd $QT_DIR/libXau
+./autogen.sh
 make install
+
+cd $QT_DIR/x11proto
+./autogen.sh
+make install
+
+cd $QT_DIR/pthread-stubs
+./autogen.sh
+make install
+
+cd $QT_DIR/proto
+./autogen.sh
+make install 
 
 cd $QT_DIR/qt-everywhere-opensource-src-5.5.0/qtbase/
 echo "Building Qt 5.5"
+pwd
 ./configure -opensource -nomake examples -nomake tests -confirm-license -qt-xcb
 make
 make install
@@ -48,6 +68,8 @@ make install
 cd $PROJ_DIR
 echo "Building qt_solar_clock"
 make 
+
+
 
 
 
