3dc5aac906520f1fe9997855b7fecc9f020fed4a
Carlos Fernandez <carlos@electricmovement.com>
2018-01-26 13:50:35 -0800

    drastically reduced build time

--- mesa-helper.sh
+++ mesa-helper.sh
@@ -6,5 +6,5 @@ include=$4
 export PKG_CONFIG_PATH=${pkgconfig_path}
 export CFLAGS=-I"${include}"
 export C_INCLUDE_PATH="${C_INCLUDE_PATH}:${include}"
-cd ${source_dir}/ && ./configure --with-dri-drivers=swrast --with-gallium-drivers=swrast  --enable-driglx-direct --enable-gles1 --enable-gles2 --enable-glx-tls --with-egl-platforms='x11 drm' --prefix=${prefix} && make  install
+cd ${source_dir}/ && ./configure --with-dri-drivers=swrast --with-gallium-drivers=swrast  --enable-driglx-direct --enable-gles1 --enable-gles2 --enable-glx-tls --with-egl-platforms='x11 drm' --prefix=${prefix} && make -j4 install
       
